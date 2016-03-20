b29f70905b2e57305beab9da0e8d92ea97800251
David De Anda <omarandstuff@gmail.com>
2016-03-19 21:57:52 -0600

    Log in via user name enabled

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  validate :validate_username
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :timeoutable
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    puts "ok"
+    if login = conditions.delete(:login)
+      conditions[:email].downcase! if conditions[:email]
+      where(conditions.to_hash).first
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_hash).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 end
